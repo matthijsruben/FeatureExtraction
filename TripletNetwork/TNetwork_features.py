f49bafca1be2c5cc9b98a7bbfb4b0b4133e06d01
reinier <reinierstribos@gmail.com>
2022-01-31 09:54:27 +0100

    Some refractoring

--- TripletNetwork/TNetwork_features.py
+++ TripletNetwork/TNetwork_features.py
@@ -8,14 +8,16 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import *
 
 data = pd.read_csv('../features.csv').values.tolist()
+# data = pd.read_csv('../features_categorised.csv').values.tolist()
 
-y = [i[0] for i in data]
-X = np.array([i[1:]for i in data])
+y = [i[0] for i in data[0:4000]]
+X = np.array([i[2:]for i in data[0:4000]])
 classes = list(set(y))
 
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.10)
 
 input_neurons = x_train.shape[1]
+output_neurons = x_train.shape[1]
 
 x_train = x_train.astype("float32")
 x_test = x_test.astype("float32")
@@ -37,9 +39,9 @@ def data_generator(batch_size=100):
 
 
 def triplet_loss(y_true, y_pred):
-    anchor_out = y_pred[:, 0:23]
-    positive_out = y_pred[:, 23:46]
-    negative_out = y_pred[:, 46:69]
+    anchor_out = y_pred[:, 0:output_neurons]
+    positive_out = y_pred[:, output_neurons:(2*output_neurons)]
+    negative_out = y_pred[:, (2*output_neurons):(3*output_neurons)]
 
     pos_dist = K.sum(K.abs(anchor_out - positive_out), axis=1)
     neg_dist = K.sum(K.abs(anchor_out - negative_out), axis=1)
@@ -51,31 +53,47 @@ def triplet_loss(y_true, y_pred):
     # pos_dist = K.sum((anchor_out - positive_out) ** 2)
     # neg_dist = K.sum((anchor_out - negative_out) ** 2)
     #
-    # delta_plus = K.pow(pos_dist, 2.7182828459) / (K.pow(pos_dist, 2.7182828459) + K.pow(neg_dist, 2.7182828459))
-    # delta_min = K.pow(neg_dist, 2.7182828459) / (K.pow(pos_dist, 2.7182828459) + K.pow(neg_dist, 2.7182828459))
-    #
-    # delta_plus = K.pow(2.7182828459, pos_dist) / (K.pow(2.7182828459, pos_dist) + K.pow(2.7182828459, neg_dist))
-    # delta_min = K.pow(2.7182828459, neg_dist) / (K.pow(2.7182828459, pos_dist) + K.pow(2.7182828459, neg_dist))
+    # delta_plus = K.exp(pos_dist) / (K.exp(pos_dist) + K.exp(neg_dist))
+    # delta_min = K.exp(neg_dist) / (K.exp(pos_dist) + K.exp(neg_dist))
     #
     # return K.sum(delta_plus + (1 - delta_min) ** 2)
+
+
+def print_accuracy(threshold, results):
+    TP, TN = 0, 0
+    FP, FN = 0, 0
+    for i in range(len(results)):
+        for j in range(len(results)):
+            similarity = K.sqrt(K.sum(K.square(model_embeddings[i] - model_embeddings[j]), axis=-1))
+            if y_test[i] == y_test[j]:
+                if similarity <= threshold:
+                    TP += 1
+                else:
+                    FP += 1
+            else:
+                if similarity > threshold:
+                    TN += 1
+                else:
+                    FN += 1
+    print("At similarity of {}: {}".format(threshold, str((TP + TN) / (TP + TN + FP + FN))))
 
 input_layer = Input(shape=(x_train.shape[1],))
 x = Dense(input_neurons, activation='relu')(input_layer)
 x = Dense(input_neurons, activation='relu')(x)
-x = Dense(input_neurons, activation='relu')(x)
+x = Dense(output_neurons, activation='relu')(x)
 model = Model(input_layer, x)
 
-model.summary()
+# model.summary()
 
 triplet_model_a = Input((input_neurons,))
 triplet_model_p = Input((input_neurons,))
 triplet_model_n = Input((input_neurons,))
 triplet_model_out = Concatenate()([model(triplet_model_a), model(triplet_model_p), model(triplet_model_n)])
 triplet_model = Model([triplet_model_a, triplet_model_p, triplet_model_n], triplet_model_out)
-triplet_model.summary()
+# triplet_model.summary()
 
 triplet_model.compile(loss=triplet_loss, optimizer="adam")
-triplet_model.fit(data_generator(), steps_per_epoch=150, epochs=50)
+triplet_model.fit(data_generator(), steps_per_epoch=200, epochs=10)
 
 model_embeddings = triplet_model.layers[3].predict(x_test, verbose=1)
 print(model_embeddings.shape)
@@ -89,49 +107,8 @@ print(model_embeddings.shape)
 #     plt.annotate(txt, (reduced_embeddings[:, 0][i], reduced_embeddings[:, 1][i]))
 # plt.show()
 
-correct_1, wrong_1 = 0, 0
-correct_2, wrong_2 = 0, 0
-correct_5, wrong_5 = 0, 0
-correct_10, wrong_10 = 0, 0
-for i in range(len(model_embeddings)):
-    for j in range(len(model_embeddings)):
-        similarity = K.sqrt(K.sum(K.square(model_embeddings[i] - model_embeddings[j]), axis=-1))
-        if y_test[i] == y_test[j]:
-            if similarity <= 1:
-                correct_1 += 1
-            else:
-                wrong_1 += 1
-            if similarity <= 2:
-                correct_2 += 1
-            else:
-                wrong_2 += 1
-            if similarity <= 5:
-                correct_5 += 1
-            else:
-                wrong_5 += 1
-            if similarity <= 10:
-                correct_10 += 1
-            else:
-                wrong_10 += 1
-        else:
-            if similarity > 1:
-                correct_1 += 1
-            else:
-                wrong_1 += 1
-            if similarity > 2:
-                correct_2 += 1
-            else:
-                wrong_2 += 1
-            if similarity > 5:
-                correct_5 += 1
-            else:
-                wrong_5 += 1
-            if similarity > 10:
-                correct_10 += 1
-            else:
-                wrong_10 += 1
-
-print("At similarity of 1: " + str(correct_1 / (correct_1 + wrong_1)))
-print("At similarity of 2: " + str(correct_2 / (correct_2 + wrong_2)))
-print("At similarity of 5: " + str(correct_5 / (correct_5 + wrong_5)))
-print("At similarity of 10: " + str(correct_10 / (correct_10 + wrong_10)))
+print_accuracy(0.5, model_embeddings)
+print_accuracy(1, model_embeddings)
+print_accuracy(2, model_embeddings)
+print_accuracy(5, model_embeddings)
+print_accuracy(10, model_embeddings)
